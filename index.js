383a66620dc8bf7a22a94bb6242d4e69b9f3b0dd
Yassir Nasri <ynasri@smb.sqli.com>
2024-03-11 10:41:10 +0100

    interface

--- index.js
+++ index.js
@@ -27,3 +27,60 @@ function greet(name) {
 function multiply(a, b) {
     return a * b;
 }
+function createEmail(to, subject) {
+    if (subject) {
+        return "Email to ".concat(to, " about ").concat(subject);
+    }
+    return "No Subject";
+}
+function add(x, y, z) {
+    if (z) {
+        return x + y + z;
+    }
+    else {
+        return x + y;
+    }
+}
+function concatenateString() {
+    var values = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        values[_i] = arguments[_i];
+    }
+    var result = "";
+    for (var _a = 0, values_1 = values; _a < values_1.length; _a++) {
+        var value = values_1[_a];
+        result += value;
+    }
+    return result;
+}
+function maxNumber() {
+    var values = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        values[_i] = arguments[_i];
+    }
+    var result = values[0];
+    for (var _a = 0, values_2 = values; _a < values_2.length; _a++) {
+        var value = values_2[_a];
+        if (value > result) {
+            result = value;
+        }
+    }
+    return result;
+}
+function createVehicle(vehicle) {
+    return vehicle;
+}
+function updatePerson(person, update) {
+    person.firstName = update.firstName;
+    person.lastName = update.lastName;
+    if (person.age && update.age) {
+        person.age = update.age;
+    }
+    if (person.email && update.email) {
+        person.email = update.email;
+    }
+    return person;
+}
+function moveCircle(circle) {
+    circle = { CenterX: 10, CenterY: 10, radius: 100 };
+}
--- index.ts
+++ index.ts
@@ -6,30 +6,110 @@ let welcomeMessage: string = "Hello, TypeScript!";
 let isLoggedIn: boolean = true;
 
 let colors: string[] = ["red", "green", "blue"];
-let userInfo : [string, number] = ["John", 25];
+let userInfo: [string, number] = ["John", 25];
 
-enum Season{
-    Spring, Summer, Autumn,Winter
+enum Season {
+    Spring, Summer, Autumn, Winter
 }
 let currentSeason: Season = Season.Spring;
 
-function logValue(value: any){
+function logValue(value: any) {
     console.log(value);
 }
 
-function noReturn():void{
+function noReturn(): void {
     console.log("No return");
 }
 
 
 
-function greet(name: string):string{
+function greet(name: string): string {
     return `Hello , ${name}`;
 }
 
 
-function multiply(a: number, b: number): number{
+function multiply(a: number, b: number): number {
     return a * b;
 }
 
-export {};
+function createEmail(to: string, subject?: string) {
+    if (subject) {
+        return `Email to ${to} about ${subject}`;
+    }
+    return "No Subject";
+}
+
+
+function add(x: number, y: number, z?: number): number {
+    if (z) {
+        return x + y + z;
+    }
+    else {
+        return x + y;
+    }
+}
+
+
+function concatenateString(...values: string[]): string {
+    let result = "";
+    for (let value of values) {
+        result += value;
+    }
+    return result;
+}
+
+
+function maxNumber(...values: number[]): number {
+    let result = values[0];
+    for (let value of values) {
+        if (value > result) {
+            result = value;
+        }
+    }
+    return result;
+}
+
+interface Vehicle {
+    make: string;
+    model: string;
+    year?: number;
+}
+
+function createVehicle(vehicle: Vehicle) {
+    return vehicle;
+}
+
+
+interface Person {
+    firstName: string;
+    lastName: string;
+    age?: number; // Optional property
+}
+
+interface Person {
+    email?: string;
+}
+
+function updatePerson(person: Person, update: Person):Person {
+    person.firstName = update.firstName;
+    person.lastName = update.lastName;
+    if (person.age && update.age) {
+        person.age = update.age;
+    }
+    if (person.email && update.email) {
+        person.email = update.email;
+    }
+    return person;
+}
+
+interface Circle{
+    readonly CenterX: number;
+    readonly CenterY: number;
+    readonly radius: number;
+}
+
+function moveCircle(circle:Circle){
+    circle={CenterX:10,CenterY:10,radius:100};
+}
+
+export { };

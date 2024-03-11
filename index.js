c39357aed88c5c070f80add5ddfb531ef405aef6
Yassir Nasri <ynasri@smb.sqli.com>
2024-03-11 10:10:28 +0100

    second

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var message = "Hello, TypeScript!";
 console.log(message);
+var temperature = 30;
+var welcomeMessage = "Hello, TypeScript!";
+var isLoggedIn = true;
+var colors = ["red", "green", "blue"];
+var userInfo = ["John", 25];
+var Season;
+(function (Season) {
+    Season[Season["Spring"] = 0] = "Spring";
+    Season[Season["Summer"] = 1] = "Summer";
+    Season[Season["Autumn"] = 2] = "Autumn";
+    Season[Season["Winter"] = 3] = "Winter";
+})(Season || (Season = {}));
+var currentSeason = Season.Spring;
+function logValue(value) {
+    console.log(value);
+}
+function noReturn() {
+    console.log("No return");
+}
+function greet(name) {
+    return "Hello , ".concat(name);
+}
+function multiply(a, b) {
+    return a * b;
+}
--- index.ts
+++ index.ts
@@ -1,2 +1,35 @@
 let message: string = "Hello, TypeScript!";
 console.log(message);
+
+let temperature: number = 30;
+let welcomeMessage: string = "Hello, TypeScript!";
+let isLoggedIn: boolean = true;
+
+let colors: string[] = ["red", "green", "blue"];
+let userInfo : [string, number] = ["John", 25];
+
+enum Season{
+    Spring, Summer, Autumn,Winter
+}
+let currentSeason: Season = Season.Spring;
+
+function logValue(value: any){
+    console.log(value);
+}
+
+function noReturn():void{
+    console.log("No return");
+}
+
+
+
+function greet(name: string):string{
+    return `Hello , ${name}`;
+}
+
+
+function multiply(a: number, b: number): number{
+    return a * b;
+}
+
+export {};
